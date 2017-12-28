d7102363599711c0c7e7cd4255e26db7814923af
Eric McDonald <3th3r3um@users.noreply.github.com>
2017-12-28 16:22:15 -0500

    Fix failing test

--- test/main.js
+++ test/main.js
@@ -2,18 +2,22 @@ var OdysseyPresaleToken = artifacts.require('OdysseyPresaleToken');
 
 contract('OdysseyPresaleToken', function(accounts) {
 
+  let contract;
+
+  before("deploy new OdysseyPresaleToken", async () => {
+      contract = await OdysseyPresaleToken.deployed();
+  });
+
   it("should put 12,000,000 tokens in the first account", async function() {
-    let numberOfTokens = 12000000
-    let contract = await OdysseyPresaleToken.deployed();
-    let balance = await contract.balanceOf.call(accounts[0]);
+    let numberOfTokens = 12000000;
+    let balance = await contract.balanceOf(accounts[0]);
     assert.equal(balance.valueOf(), numberOfTokens, "12,000,000 wasn't in the first account.")
   });
 
   it("should transfer 3,000 tokens to an arbitrary account", async function() {
     let numberOfTokens = 3000;
     let recipient = '0xB03e2995D8a57D5558262BE381733E708202acdD';
-    let contract = await OdysseyPresaleToken.deployed();
-    let result = await contract.transfer.call(recipient, numberOfTokens, {from: accounts[0]});
+    let result = await contract.transfer(recipient, numberOfTokens, {from: accounts[0], gas: 250000});
     let recipientBalance = await contract.balanceOf.call(recipient);
     assert.equal(recipientBalance.valueOf(), numberOfTokens, "Recipient balance not updated after token transfer.");
   });
